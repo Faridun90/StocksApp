86491d1c3d7085b4579555030a300724baca3cf2
Faridun Mirzoev <faridun.mirzoev@gmail.com>
2024-05-02 12:16:45 -0700

    retrieve MyStocks data on portfolio page

--- src/views/MyStocks.vue
+++ src/views/MyStocks.vue
@@ -1,6 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useStocksStore } from '@/stores/stocks'
+import { onMounted } from 'vue'
+const stocksStore = useStocksStore()
+const myStocks = stocksStore.myStocks
+onMounted(async () => {
+  try {
+    await initialize()
+  } catch (error) {
+    console.error('Error fetching stocks:', error)
+  }
+})
+const initialize = async () => {
+  stocksStore.initialize()
+}
+</script>
 <template>
-  <div class="bg-black w-full h-screen">
-    <h1 class="text-white">My Stocks</h1>
+  <div class="text-white bg-black w-full h-screen">
+    <h1 class="">My Stocks</h1>
+    <h3 v-for="(stock, index) in myStocks" :key="index">{{ stock }}</h3>
   </div>
 </template>
