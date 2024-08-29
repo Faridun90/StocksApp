0b99e6f27a2671ebc30281c5ecd3f167312385d4
Faridun Mirzoev <faridun.mirzoev@gmail.com>
2024-08-29 00:11:47 -0700

    change home page to "stocks"

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'stocks',
+      component: () => import('../views/BuyStocks.vue')
     },
     {
       path: '/calculator',
       name: 'calculator',
       component: () => import('../views/CalculatorView.vue')
-    },
-    {
-      path: '/stocks',
-      name: 'stocks',
-      component: () => import('../views/BuyStocks.vue')
     },
     {
       path: '/stocks/my-portfolio',
